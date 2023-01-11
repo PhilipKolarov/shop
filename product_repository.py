23aa2ff1a6d1f0d5ad6937fbfd72e9ec084f2f73
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-01-11 23:45:24 +0200

    Add files via upload

---
+++ product_repository.py
@@ -0,0 +1,26 @@
+from project_exam.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                self.products.remove(p)
+
+    def __repr__(self):
+        result = ''
+        for p in self.products:
+            result += f"{p.name}: {p.quantity}\n"
+
+        return result.strip()
